e2e702ab7238004b700757b6c26341819aec7fde
sliu008 <69875423+sliu008@users.noreply.github.com>
2023-05-26 08:31:56 -0700

    Feature/podaac 5537 (#161)
    * update l2ss py to open hdf5 files if we fail to open as netcdf, and change way we copy attributes in case it has a /
    
    * fix pylint and removed comment
    
    * update changelog
    
    * change so that were able to process S1 and S2 dtypes
    
    * fix pylint and flake8
    
    * revert spaces
    
    * remove spaces
    
    * remove spaces

--- podaac/subsetter/group_handling.py
+++ podaac/subsetter/group_handling.py
@@ -187,6 +187,8 @@ def _rename_variables(dataset: xr.Dataset, base_dataset: nc.Dataset, start_date)
             var_data = np.array(variable.data)
         elif variable.dtype == 'timedelta64[ns]':
             var_group.createVariable(new_var_name, 'i4', var_dims, fill_value=fill_value, **comp_args)
+        elif variable.dtype in ['|S1', '|S2']:
+            var_group.createVariable(new_var_name, variable.dtype, var_dims, fill_value=fill_value)
         else:
             var_group.createVariable(new_var_name, variable.dtype, var_dims, fill_value=fill_value, **comp_args)
 
@@ -195,7 +197,10 @@ def _rename_variables(dataset: xr.Dataset, base_dataset: nc.Dataset, start_date)
 
         # Copy data
         var_group.variables[new_var_name].set_auto_maskandscale(False)
-        var_group.variables[new_var_name][:] = var_data
+        if variable.dtype in ['|S1', '|S2']:
+            var_group.variables[new_var_name][:] = variable.values
+        else:
+            var_group.variables[new_var_name][:] = var_data
 
 
 def h5file_transform(finput: str) -> Tuple[nc.Dataset, bool]:
