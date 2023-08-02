224fe7022c77cb4d30ccbb7912de6f3a2dd5b542
nlensse1 <nicholas.f.lenssen@nasa.gov>
2023-08-02 13:55:08 -0400

    Updated comments

--- podaac/subsetter/gpm_cleanup.py
+++ podaac/subsetter/gpm_cleanup.py
@@ -13,15 +13,18 @@ def change_var_dims(nc_dataset, variables=None):
     dimensions to have the name in the DimensionName attribute rather than phony_dim
     """
     var_list = list(nc_dataset.variables.keys())
+    # loop through variable list to avoid netcdf4 runtime error
     for var_name in var_list:
         # GPM will always need to be cleaned up via netCDF
         # generalizing coordinate variables in netCDF file to speed variable subsetting up
         if variables:
             if var_name not in variables and 'lat' not in var_name.lower() and 'lon' not in var_name.lower() and 'time' not in var_name.lower():
+                # delete the uneccesary variables
                 del nc_dataset.variables[var_name]
                 continue
 
         var = nc_dataset.variables[var_name]
+        # get the DimensionName attribute from the variable
         if 'DimensionNames' in var.ncattrs():
             dim_list = var.getncattr('DimensionNames').split(',')
             # create dimension map
@@ -33,12 +36,13 @@ def change_var_dims(nc_dataset, variables=None):
                 length = var.shape[count]
                 # check if the dimension name created has already been created in the dataset
                 if new_dim not in dim_dict:
+                    # create the new dimension
                     nc_dataset.createDimension(new_dim, length)
                     dim_dict[new_dim] = length
-
+            # utilized from Dimension Cleanup module
             attrs_contents = {}
             new_mapped_var = {}
-
+            # if the variable has attributes, get the attributes to then be copied to the new variable
             if len(var.ncattrs()) > 0:
                 for attrname in var.ncattrs():
                     if attrname != '_FillValue':
@@ -46,13 +50,16 @@ def change_var_dims(nc_dataset, variables=None):
 
                 fill_value = var._FillValue  # pylint: disable=W0212
                 dim_tup = ('__'+dim_prefix+'__'+i for i in var.getncattr('DimensionNames').split(','))
+                # delete the old variable with phony_dim names
                 del nc_dataset.variables[var_name]
 
+                # create the new variable with dimension names
                 new_mapped_var[var_name] = nc_dataset.createVariable(var_name, str(var[:].dtype),
                                                                      dim_tup, fill_value=fill_value)
                 for attr_name, contents in attrs_contents.items():
                     new_mapped_var[var_name].setncattr(attr_name, contents)
 
+                # copy the data to the new variable with dimension names
                 new_mapped_var[var_name][:] = var[:]
 
     return nc_dataset
