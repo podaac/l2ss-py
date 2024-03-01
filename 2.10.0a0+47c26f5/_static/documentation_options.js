4f087dc323d1788a707642f3e6b5e0fb62c68b7b
nlenssen2013 <nicklenssen4@gmail.com>
2024-03-01 21:49:11 +0000

    Deploying to gh-pages from @ podaac/l2ss-py@47c26f5c0f197771adf606174f0fa22d84b54830 🚀

---
+++ 2.10.0a0+47c26f5/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.10.0a0+47c26f5',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
