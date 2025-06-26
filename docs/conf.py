4ecd92aabc44d6793e2287430c3f82a727ee1915
Simon Liu <simon.liu@jpl.nasa.gov>
2025-06-26 10:54:36 -0700

    update sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,8 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
